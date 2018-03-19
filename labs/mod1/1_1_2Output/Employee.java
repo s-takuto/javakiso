c1b0d6d34009cad819cd4d8cfcd8fcb65417b37f
s_takuto <s_takuto@cs.meiji.ac.jp>
2018-03-19 19:31:31 +0900

    Add

---
+++ labs/mod1/1_1_2Output/Employee.java
@@ -0,0 +1,5 @@
+public class Employee {
+	public static void main(String[] args) {
+		System.out.println("従業員番号:11001  名前:鈴木");
+	}
+}
---
+++ labs/mod1/1_1_3Introduction/Introduction.java
@@ -0,0 +1,9 @@
+public class Introduction {
+	public static void main(String[] args) {
+		System.out.println("会社名 : NSソリューションズ東京");
+		System.out.println("氏名 : 佐藤拓人");
+		System.out.println("所属 : 決まっていない");
+		System.out.println("内線 : 決まっていない");
+		System.out.println("一言 : Javaの思想をきちんと理解する！");
+	}
+}
